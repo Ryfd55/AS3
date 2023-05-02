32cded6b9c57abd60c5bda10566c4950c5d9fa0b
Denis Gubardin <gubardin1984@mail.ru>
2023-05-02 23:50:34 +0200

    HW2.1 1000 to 1K

---
+++ app/src/main/java/ru/netology/nmedia3/dto/Post.kt
@@ -0,0 +1,12 @@
+package ru.netology.nmedia3.dto
+
+data class Post(
+    val id: Long,
+    val author: String,
+    val published: String,
+    val content: String,
+    var likedByMe: Boolean = true,
+    var likes: Long = 999,
+    var sharesCount: Long = 999998
+)
+
